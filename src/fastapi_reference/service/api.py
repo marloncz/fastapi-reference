39d0a354b177a3c29552cc2b449372201f41fe9a
marloncz <m.c.schumacher@live.de>
2024-11-29 13:00:05 +0100

    feat(api): endpoint for product update

--- src/fastapi_reference/service/api.py
+++ src/fastapi_reference/service/api.py
@@ -2,9 +2,10 @@ import logging
 from datetime import date
 from typing import cast
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy import create_engine
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import sessionmaker
 
 from fastapi_reference.data.mock_data import generate_mock_data
@@ -14,6 +15,7 @@ from fastapi_reference.utils.models import (
     Product,
     RandomSample,
     ReceiveProducts,
+    UpdateProducts,
 )
 from fastapi_reference.utils.sql import DATABASE_URL, ProductTable
 
@@ -81,6 +83,46 @@ async def receive_products_by_id(body: GetProductsRequest):
     ]
 
     return ReceiveProducts(products=[Product(**product) for product in products_list])  # type: ignore
+
+
+@app.post("/update_products", status_code=200, response_model=GeneralResponse)
+async def update_products(body: UpdateProducts):
+    """Endpoint for updating products."""
+    engine = create_engine(DATABASE_URL)
+    session_maker = sessionmaker(bind=engine)
+    session = session_maker()
+    try:
+        missing_products = []
+        for product in body.products:
+            db_product = (
+                session.query(ProductTable)
+                .filter(ProductTable.product_id == product.product_id)
+                .first()
+            )
+
+            if db_product:
+                db_product.product_name = product.product_name
+                db_product.product_description = product.product_description
+                db_product.product_category = product.product_category
+                # NOTE: weird mypy issue here...
+                db_product.price = float(db_product.price)  # type: ignore
+                db_product.price = product.price  # type: ignore
+                db_product.stock = product.stock
+                db_product.valid_from = product.valid_from
+                db_product.valid_to = product.valid_to
+                db_product.user_email = body.user_name
+                db_product.user_name = body.user_email
+                session.commit()
+            else:
+                missing_products.append(product.product_id)
+    except SQLAlchemyError as e:
+        logger.error("Error: %s", e)
+        session.rollback()
+        raise HTTPException(
+            status_code=500, detail=f"An error occurred while updating products: {str(e)}"
+        ) from e
+
+    return GeneralResponse(message="Products updated successfully.")
 
 
 if __name__ == "__main__":
